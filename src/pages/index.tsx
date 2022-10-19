da6a63ebe2ab7f6ebe5cf978569722c6bc753a10
Tomas Hein <theinfx@gmail.com>
2022-10-19 19:57:06 -0300

    cleanup and base configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Onesta</title>
+        <meta name="description" content="Test app development with NextJS and Tailwind CSS" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
